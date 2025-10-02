61d203bd9a2e7937e5838ac55823ff11ebdda3a5
a637814143 <140132912+a637814143@users.noreply.github.com>
2025-10-02 17:58:55 +0800

    Add user login credentials table

--- silkmall.sql
+++ silkmall.sql
@@ -17,4 +17,18 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+DROP TABLE IF EXISTS `user_account`;
+CREATE TABLE `user_account`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `login_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '登录名',
+  `login_password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '登录密码',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `uk_user_account_login_name`(`login_name`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+INSERT INTO `user_account` (`login_name`, `login_password`)
+VALUES
+  ('admin', 'admin123');
+
 SET FOREIGN_KEY_CHECKS = 1;
